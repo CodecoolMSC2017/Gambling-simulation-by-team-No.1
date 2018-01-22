6782e18bdd1f5efd31d3ef9de974084be8ef053c
Gergely Murvai <murvai.gergely@gmail.com>
2018-01-22 15:32:15 +0100

    Basic implemet of Result class

---
+++ Result.java
@@ -0,0 +1,9 @@
+public class Result {
+    Pilot first;
+    Statistics stats;
+
+    public Result(Pilot first, Statistic stats){
+        this.first = first;
+        this.stats = stats;
+    }
+}
