3e6d9d8979e8166cbeadebda9a52dc05e5435ec7
Gergely Murvai <murvai.gergely@gmail.com>
2018-01-23 14:59:23 +0100

    run() method implemented

--- Simulator.java
+++ Simulator.java
@@ -8,4 +8,11 @@ public class Simulator {
         this.simulation = simulation;
         this.logger = logger;
     }
+    public Result run(){
+        Race race = new Race();
+        Simulation simul = new Simulation(race.runRace());
+        Statictics stats = new Statictics(simul.getPilotArr());
+        return new Result(race.FirstSix(simul.getPilotArr()), stats);
+
+    }
 }
