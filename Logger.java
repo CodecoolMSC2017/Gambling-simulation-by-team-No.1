644c1967662d6791b3203b5602093b7fe90adacb
Gergely Murvai <murvai.gergely@gmail.com>
2018-01-24 11:08:55 +0100

    printMenu(),clearScreen() and basicPrinter(str) methods are created

--- Logger.java
+++ Logger.java
@@ -3,4 +3,24 @@ public class Logger {
     public void Log(String type, String message){
         System.out.println(type + " " + message);
     }
+    
+    public static void printMenu() {
+        System.out.println("Welcome to Formula 1 betting simmulator\n");
+        System.out.println("Menu\n");
+        System.out.println(":winners");
+        System.out.println(":funfacts");
+        System.out.println(":stats");
+        System.out.println("\nChoose :\n");
+    }
+    
+    public static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }
+    
+    public static void basicPrinter(String str) {
+        clearScreen();
+        System.out.println(str);
+        Main.menus();
+    }
 }
